5fef0121a927142d294f97486ca2035315abe493
TrongHungHuynh <huynhtronghung81199@gmail.com>
2020-11-03 22:33:20 +0700

    ..

--- routes/index.js
+++ routes/index.js
@@ -9,22 +9,17 @@ router.get('/', function(req, res, next) {
 router.post('/', (req, res)=> {
   const firstNumber = parseFloat(req.body.firstNumber);
   const secondNumber = parseFloat(req.body.secondNumber);
-  const operators = req.body.operation;
+  const operator = req.body.operation;
   let result;
-  for (let i = 0; i < operators.length; i++) {
-    if (operators[i].checked) {
-      const operator = operators[i];
-      if (operator == 'add')
-        result = firstNumber + secondNumber;
-      else if (operator == 'subtract')
-        result = firstNumber - secondNumber;
-      else if (operator == 'multiple')
-        result = firstNumber * secondNumber;
-      else if (operator == 'divide')
-        result = firstNumber / secondNumber;
-    }
-  }
-  res.render('index', { title: 'Express', firstNumber, secondNumber, operators, result });
+  if (operator == 'add')
+    result = firstNumber + secondNumber;
+  else if (operator == 'subtract')
+    result = firstNumber - secondNumber;
+  else if (operator == 'multiple')
+    result = firstNumber * secondNumber;
+  else if (operator == 'divide')
+    result = firstNumber / secondNumber;
+  res.render('index', { title: 'Express', firstNumber, secondNumber, operator, result });
 });
 
 module.exports = router;
