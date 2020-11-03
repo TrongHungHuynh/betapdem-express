e13a6a4bba8f81db3b6e8485f9dc2dd6ad8fa5ac
TrongHungHuynh <huynhtronghung81199@gmail.com>
2020-11-03 21:28:45 +0700

    Update js

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,6 +1,7 @@
 function checkFunction() {
-    let firstNumber = document.getElementById('firstNumber').value;
-    let secondNumber = document.getElementById('secondNumber').value;
+    const firstNumber = document.getElementById('firstNumber').value;
+    const secondNumber = document.getElementById('secondNumber').value;
+
     if (firstNumber.length == 0) {
         document.getElementById("warning").innerHTML = "Chưa nhập số thứ nhất."
         return false;
@@ -16,46 +17,29 @@ function checkFunction() {
         document.getElementById("warning").innerHTML = "Số thứ hai không phải là số thực."
         return false;
     }
+
     document.getElementById("warning").innerHTML = ""
     return true;
+
 }
 
-function calculate() {
+function checkOperator() {
     if (checkFunction() == false) {
         return false;
     }
-    const firstNumber = parseFloat(document.getElementById('firstNumber').value);
-    const secondNumber = parseFloat(document.getElementById('secondNumber').value);
+
     const operators = document.getElementsByName('operation');
-    let result;
-
-    while (true) {
-        let count = 0;
-        for (let i = 0; i < operators.length; i++) {
-            if (operators[i].checked) {
-                count++;
-            }
-        }
-        if (count != 1) {
-            document.getElementById("warning").innerHTML = "Chưa chọn phép tính."
-            return false;
-        }
-        document.getElementById("warning").innerHTML = ""
-        break;
-    }
-
+    let count = 0;
     for (let i = 0; i < operators.length; i++) {
         if (operators[i].checked) {
-            const operator = operators[i].value;
-            if (operator == 'add')
-                result = firstNumber + secondNumber;
-            else if (operator == 'subtract')
-                result = firstNumber - secondNumber;
-            else if (operator == 'multiple')
-                result = firstNumber * secondNumber;
-            else if (operator == 'divide')
-                result = firstNumber / secondNumber;
+            count++;
         }
     }
-    document.getElementById("result").innerHTML = result.toString();
+    if (count != 1) {
+        document.getElementById("warning").innerHTML = "Chưa chọn phép tính."
+        return false;
+    }
+
+    document.getElementById("warning").innerHTML = ""
+    return true;
 }
--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,24 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', (req, res)=> {
-  const number = req.body.number;
-  const result = Math.pow(parseFloat(number),2);
-  res.render('index', { title: 'Express', number, result });
+  const firstNumber = parseFloat(req.document.getElementById('firstNumber').value);
+  const secondNumber = parseFloat(req.document.getElementById('secondNumber').value);
+  const operators = req.document.getElementsByName('operation');
+  let result;
+  for (let i = 0; i < operators.length; i++) {
+    if (operators[i].checked) {
+      const operator = operators[i].value;
+      if (operator == 'add')
+        result = firstNumber + secondNumber;
+      else if (operator == 'subtract')
+        result = firstNumber - secondNumber;
+      else if (operator == 'multiple')
+        result = firstNumber * secondNumber;
+      else if (operator == 'divide')
+        result = firstNumber / secondNumber;
+    }
+  }
+  res.render('index', { title: 'Express', firstNumber, secondNumber, operators, result });
 });
 
 module.exports = router;
